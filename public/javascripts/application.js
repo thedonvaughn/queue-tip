d700230ab0cbafe45dd7cd0e68344576d067210b
Jayson Vaughn <vaughn.jayson@gmail.com>
2009-01-28 20:40:11 -0600

    Removed javascript code from layout into application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,29 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function ToggleHeader()
+{
+    if( $("headster").style.display == 'block') 
+    {
+      $("headster").style.display = 'none';
+    } 
+    else
+    {
+      $("headster").style.display ='block';
+    }
+    if( $("headster2").style.display == 'block') 
+    {
+      $("headster2").style.display = 'none';
+    } 
+    else
+    {
+      $("headster2").style.display ='block';
+    }
+    if( $("footster").style.display == 'block') 
+    {
+      $("footster").style.display = 'none';
+    } 
+    else
+    {
+      $("footster").style.display ='block';
+    }
+}
