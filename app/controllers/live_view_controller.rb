2078436c3995d4cbf89c83ce92eeab3efb12de89
Jayson Vaughn <jayson@onedrop.(none)>
2009-05-08 08:34:36 -0500

    Migrating to Rails 2.3 branch  * Moved controllers/application.rb to application_controller.rb  * Made sure 'drb' is required in LiveView controller for proper error  * handling  * Updated config/environment.rb to require Rails 2.3.2

--- app/controllers/live_view_controller.rb
+++ app/controllers/live_view_controller.rb
@@ -16,6 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Queue-Tip.  If not, see <http://www.gnu.org/licenses/>.
 #
+require 'drb'
 
 class LiveViewController < ApplicationController
   before_filter :establish_connection
