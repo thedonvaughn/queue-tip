8641edf2989bd815b456d097efcea33d63fb53f2
Peter Bex <peter@pebble-software.nl>
2016-05-05 14:10:15 +0200

    Use new attributes hash format
    The format of the attributes has changed several times in Adhearsion.

--- bin/ami_proxy/lib/queue_status.rb
+++ bin/ami_proxy/lib/queue_status.rb
@@ -1,9 +1,17 @@
 module QueueStatus
+  # Like attributes_hash but without snake_case and to_symbol
+  def get_attributes_hash(attributes)
+    attributes.inject({}) do |hash, attribute|
+      hash[attribute.name] = attribute.value
+      hash
+    end
+  end
+
   def queue_status
     #if Adhearsion::Asterisk.manager_interface
     prag_hash = {}
     Adhearsion::Asterisk.execute_ami_action('QueueStatus') do |x|
-      h = x.headers
+      h = get_attributes_hash(x.attributes)
       prag_hash[h['Queue']] = { :members => {}, :callers => {} } .merge(h) if x.name =~ /QueueParams/i
       prag_hash[h['Queue']][:members].merge!(h['Name'] => {}.merge(h)) if x.name =~ /QueueMember/i
       prag_hash[h['Queue']][:callers].merge!(h['Position'] => {}.merge(h)) if x.name =~ /QueueEntry/i
